677c4157610f0615ddb067278bb5b58b070c9c4a
Jahmere Fernandez <jahmeref2@nycstudents.net>
2026-05-29 12:03:50 -0400

    Almost done part 1

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ submit.addEventListener("click", function(e) {
   
   
     // localStorage here
-  
+  const date = document.getElementById('date').value;
+    localStorage.setItem("local storage", date);
+
   
   
   // sessionStorage here
+const riddle = document.getElementById('riddle').value;
+    sessionStorage.setItem("riddle", riddle);
 
+
+        console.log(sessionStorage.getItem("riddle"));
 
 });
